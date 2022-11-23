53c4ae17a6a32a8f4666aa003cd6a5cc1535d14a
Gerardo Salinas <gerardoaplicano16@gmail.com>
2022-11-23 09:16:14 -0600

    Creados Ficheros de Estructuras

---
+++ Estructura Lista Enlazada/PruebaLista.java
@@ -0,0 +1,23 @@
+public class PruebaLista {
+    
+
+    public static void main(String[] args) {
+        
+        Lista nvaLista = new Lista("paises");
+
+        
+        nvaLista.insertarAlFinal("C");        
+        nvaLista.insertarAlFinal("A");        
+        nvaLista.insertarAlFinal("T");
+        nvaLista.insertarAlFinal("A");
+        nvaLista.insertarAlFinal("R");
+        nvaLista.insertarAlFrente("2022_");
+        nvaLista.imprimirLista();
+       
+        
+
+       
+
+
+    }
+}
